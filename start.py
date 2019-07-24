44b67111c7da1f290b04fe39010104587223121b
teerryn <slaughterer90@gmail.com>
2019-07-24 20:22:12 +0100

    add print glyph functionality

--- start.py
+++ start.py
@@ -1,11 +1,33 @@
 import json
 
 
-def main():
+def print_result(number_to_print):
+    """
+    Takes a string of numbers and prints the equivalent glyph.
+    :param number_to_print: Number to be printed into a glyph.
+    """
     data = {}
+    glyphList = []
     with open("glyph.json") as outfile:
         data = json.load(outfile)
 
+    for letter in number_to_print:
+        glyphList.append(data[letter].split("\n"))
+        glyphList.append(data[" "].split("\n"))
+
+    print_glyphs(glyphList)
+
+
+def print_glyphs(listOfLetters):
+    """
+    Prints into the screen the combination of a list of glyphs.
+    :param listOfLetters: List of glyphs to be printed
+    """
+    for row in list(zip(*listOfLetters)):
+        text = ""
+        for letter in row:
+            text = text + letter
+        print(text)
 
 
 def solve_for_i(p, r, t):
